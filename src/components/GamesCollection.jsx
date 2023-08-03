ade24d4fa1dabb3d4d5c1d8162ceee398b1057ea
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-08-03 18:01:03 +0300

    Extract new component from ProductsPage.jsx

---
+++ src/components/GamesCollection.jsx
@@ -0,0 +1,70 @@
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useProductsData } from "./utils/dataContext";
+import "../styles/GamesCollection.css";
+
+const Card = ({ id, imageURL, name, price, onClick }) => {
+  return (
+    <div className="productCard" id={id} onClick={onClick}>
+      <img src={imageURL} alt={`${name} image`}></img>
+      <div className="cardDescription">
+        <p>{name}</p>
+        <p>{`$${price}`}</p>
+      </div>
+    </div>
+  );
+};
+
+const Collection = () => {
+  const data = useProductsData();
+  const array = Object.values(data);
+  const navigate = useNavigate();
+
+  const getGameData = (id) => {
+    for (let i = 0; i < array.length; i++) {
+      const obj = array[i];
+      if (obj.data.uniqueId === id) {
+        return obj;
+      }
+    }
+  };
+
+  const handleClick = (e) => {
+    const gameId = parseInt(e.target.id);
+    const gameData = getGameData(gameId);
+    const navigationName = gameData.data.slug;
+    console.log(navigationName);
+
+    navigate("/games/game", { state: gameData });
+  };
+
+  return (
+    <div className="gamesCollection">
+      <h1>All Games</h1>
+      <div className="collection">
+        {array.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              id={item.data.uniqueId}
+              imageURL={item.data.imageURL}
+              name={item.data.name}
+              price={item.data.price}
+              onClick={handleClick}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  imageURL: PropTypes.string,
+  name: PropTypes.string,
+  price: PropTypes.number,
+  onClick: PropTypes.func,
+  id: PropTypes.number,
+};
+
+export default Collection;
--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,51 +1,12 @@
-import PropTypes from "prop-types";
-import { useProductsData } from "./DataProvider";
+import { Outlet } from "react-router-dom";
 import "../styles/ProductsPage.css";
 
-const priceData = [
-  59.99, 59.99, 49.99, 29.99, 49.99, 29.99, 69.99, 10.0, 9.99, 69.99, 59.99,
-  29.99, 29.99, 59.99, 29.99, 19.99, 59.99, 59.99, 69.99, 10,
-];
-
-const Card = ({ imageURL, name, price }) => {
-  return (
-    <div className="productCard">
-      <img src={imageURL} alt={`${name} image`}></img>
-      <div className="cardDescription">
-        <p>{name}</p>
-        <p>{`$${price}`}</p>
-      </div>
-    </div>
-  );
-};
-
 const ProductsPage = () => {
-  const data = useProductsData();
-  const array = Object.values(data);
-
   return (
     <div className="productsPage">
-      <h1>All Games</h1>
-      <div className="collection">
-        {array.map((item, index) => {
-          return (
-            <Card
-              key={item.data.uniqueId}
-              imageURL={item.data.imageURL}
-              name={item.data.name}
-              price={priceData[index]}
-            />
-          );
-        })}
-      </div>
+      <Outlet />
     </div>
   );
-};
-
-Card.propTypes = {
-  imageURL: PropTypes.string,
-  name: PropTypes.string,
-  price: PropTypes.number,
 };
 
 export default ProductsPage;

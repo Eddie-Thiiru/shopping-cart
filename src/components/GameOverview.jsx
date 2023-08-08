9b140f89d0ff584d83b8a07cac636aa956310905
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-08-08 13:45:38 +0300

    Update products page layout

--- src/components/GameOverview.jsx
+++ src/components/GameOverview.jsx
@@ -23,7 +23,7 @@ const Overview = () => {
           <p>{data.story}</p>
         </div>
         <div className="gameMeta">
-          <div className="gameGenreScore">
+          <div className="gameMetaOne">
             <div className="genre">
               <h4>Genre</h4>
               <p>{data.genre.map((item) => `${item.name} `)} </p>
@@ -33,19 +33,26 @@ const Overview = () => {
               <p>{data.metaScore}</p>
             </div>
           </div>
-          <div className="releaseDate">
-            <h4>Release Date</h4>
-            <p>{data.released}</p>
-          </div>
-          <div className="gamePlatforms">
-            <p>{data.platforms.map((item) => `${item.platform.name} `)}</p>
+          <div className="gameMetaTwo">
+            <div className="gamePlatforms">
+              <h4>Platforms</h4>
+              <ul>
+                {data.platforms.map((item, index) => (
+                  <li key={index}>{item.platform.name}</li>
+                ))}
+              </ul>
+            </div>
+            <div className="releaseDate">
+              <h4>Release Date</h4>
+              <p>{data.released}</p>
+            </div>
           </div>
         </div>
       </div>
       <div className="gameDetailsTwo">
         <h2>{data.name}</h2>
         <div className="shopSection">
-          <p>{data.price}</p>
+          <p>{`$${data.price}`}</p>
           <div className="cartBuy">
             <button type="button" onClick={() => addToCart(data)}>
               ADD TO CART
